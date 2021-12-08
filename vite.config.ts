1eb013e78ad16282c1e03d079c5be5cc198558d3
Romuald Brillout <git@brillout.com>
2021-12-08 17:21:26 +0100

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import mdx from 'vite-plugin-mdx'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
-import { mdxExportHeadings } from './vite.config/vite-plugin-mdx-export-headings'
 import low from 'lowlight'
 import { hljsDefineVue } from './vite.config/highlight.js/vue'
 // @ts-ignore
@@ -14,8 +13,8 @@ low.registerLanguage('js', hljsDefineJavascript)
 const remarkPlugins = [require('remark-highlight.js')]
 
 const config: UserConfig = {
-  root: `${__dirname}/../../docs`,
-  plugins: [react(), mdxExportHeadings(), mdx({ remarkPlugins }), ssr()],
+  root: __dirname,
+  plugins: [react(), mdx({ remarkPlugins }), ssr()],
   optimizeDeps: { include: ['@mdx-js/react'] },
   clearScreen: false,
 }
