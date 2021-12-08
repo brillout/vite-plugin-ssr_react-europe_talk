e8aa770eefb3576de9d7515244cc2f743f9a1eac
Romuald Brillout <git@brillout.com>
2021-12-08 19:08:17 +0100

    wip

---
+++ demo/pages/index/index.page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Counter } from './Counter'
+
+export { Page }
+
+function Page() {
+  return (
+    <>
+    <h1>Demo: SSR</h1>
+    <ul>
+    <li>
+     Rendered to HTML: yes.
+    </li>
+    <li>
+    Interactive: yes.
+    </li>
+    </ul>
+    <div>
+    Counter: <Counter />
+    </div>
+    </>
+  )
+}
