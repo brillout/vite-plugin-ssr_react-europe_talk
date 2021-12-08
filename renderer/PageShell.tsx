c9ea947ec4f30f57e8a05ba9334138f5f108708d
Romuald Brillout <git@brillout.com>
2021-12-08 16:55:20 +0100

    wip

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -11,18 +11,7 @@ function PageShell({ children, pageContext }: { children: React.ReactNode; pageC
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
-        <Layout>
-          <Sidebar>
-            <Logo />
-            <Link className="navitem" href="/">
-              Home
-            </Link>
-            <Link className="navitem" href="/about">
-              About
-            </Link>
-          </Sidebar>
-          <Content>{children}</Content>
-        </Layout>
+        <Layout>{children}</Layout>
       </PageContextProvider>
     </React.StrictMode>
   )
@@ -35,38 +24,6 @@ function Layout({ children }: { children: React.ReactNode }) {
         display: 'flex',
         maxWidth: 900,
         margin: 'auto',
-      }}
-    >
-      {children}
-    </div>
-  )
-}
-
-function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        lineHeight: '1.8em',
-      }}
-    >
-      {children}
-    </div>
-  )
-}
-
-function Content({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh',
       }}
     >
       {children}
