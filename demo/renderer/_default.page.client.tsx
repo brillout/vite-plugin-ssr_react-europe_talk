47442dd2a148c699d8ae4fd22040429eb16bcfca
Romuald Brillout <git@brillout.com>
2021-12-09 08:26:52 +0100

    wip

--- demo/renderer/_default.page.client.tsx
+++ demo/renderer/_default.page.client.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import { getPage } from 'vite-plugin-ssr/client'
-import { PageShell } from './PageShell'
 import type { PageContext } from './types'
 import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client'
 
@@ -13,9 +12,7 @@ async function hydrate() {
   const pageContext = await getPage<PageContextBuiltInClient & PageContext>()
   const { Page, pageProps } = pageContext
   ReactDOM.hydrate(
-    <PageShell pageContext={pageContext}>
-      <Page {...pageProps} />
-    </PageShell>,
+      <Page {...pageProps} />,
     document.getElementById('page-view'),
   )
 }
