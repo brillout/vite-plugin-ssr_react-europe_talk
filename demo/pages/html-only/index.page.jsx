6fdef9a42278f96a0603be0f56a06ccc0ec988bd
Romuald Brillout <git@brillout.com>
2021-12-09 14:31:42 +0100

    wip

--- demo/pages/html-only/index.page.jsx
+++ demo/pages/html-only/index.page.jsx
@@ -7,6 +7,7 @@ function Page() {
   return (
     <>
       <h1>Demo: SSR</h1>
+      { new Date().toString() }
       <ul>
         <li>Rendered to HTML?</li>
         <li>Interactive?</li>
--- demo/server/index.js
+++ demo/server/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import { createPageRenderer } from 'vite-plugin-ssr'
+const express = require('express')
+const { createPageRenderer } = require('vite-plugin-ssr')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
